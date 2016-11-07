650a58b578fb9e87c52fcec89c7b36b64ed09be6
Estela Diaz <diazsanchez.estela@gmail.com>
2016-11-07 10:14:57 -0500

    adding pseudo code

--- HW4/trafficLight/index.js
+++ HW4/trafficLight/index.js
@@ -1,34 +1,35 @@
+// if STOP light clicked
 document.getElementById('stopButton').onclick = illuminateRed;
-
+// clear lights and make STOP red
 function illuminateRed() {
   clearLightsStopSel();
   document.getElementById('stopLight').style.backgroundColor = "red";
 }
-
+// clears all lights but the "STOP"
 function clearLightsStopSel() {
   document.getElementById('slowLight').style.backgroundColor = "black";
   document.getElementById('goLight').style.backgroundColor = "black";
 }
-
+// if SLOW light clicked
 document.getElementById('slowButton').onclick=illuminateYellow;
-
+// illuminate SLOW light
 function illuminateYellow() {
 	clearLightsSlowSel();
 	document.getElementById('slowLight').style.backgroundColor="yellow";
 }
-
+// clears all lights but the "SLOW"
 function clearLightsSlowSel() {
   document.getElementById('stopLight').style.backgroundColor = "black";
   document.getElementById('goLight').style.backgroundColor = "black";
 }
-
+// if GO light clicked
 document.getElementById('goButton').onclick=illuminateGreen;
-
+// clear lights and make GO green
 function illuminateGreen() {
 	clearLightsGoSel();
 	document.getElementById('goLight').style.backgroundColor="green";
 }
-
+// clears all lights but the "GO"
 function clearLightsGoSel() {
   document.getElementById('stopLight').style.backgroundColor = "black";
   document.getElementById('slowLight').style.backgroundColor = "black";
